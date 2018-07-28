99839635a10444c78cbc2e7ce693328f0fdbc7c4
kiiruha <k.vozniy@gmail.com>
2018-07-28 20:04:42 +0300

    Added getWeb3.js and connected it into index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
+import getWeb3 from '../util/getWeb3'
 Vue.use(Vuex)
 export const store = new Vuex.Store({
  strict: true,
  state,
+ getWeb3,
  mutations: {},
  actions: {}
 })
