a59d3a2543e3d755ad5bfb9db9a83cabd0095e36
kiiruha <k.vozniy@gmail.com>
2018-07-28 19:48:56 +0300

    Configured Vue  || Added hello-metamask and casino-dapp

---
+++ src/components/casino-dapp.vue
@@ -0,0 +1,14 @@
+<template>
+ <hello-metamask/>
+</template>
+<script>
+import HelloMetamask from '@/components/hello-metamask'
+export default {
+ name: 'casino-dapp',
+ components: {
+ 'hello-metamask': HelloMetamask
+ }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/components/hello-metamask.vue
@@ -0,0 +1,10 @@
+<template>
+ <p>Hello</p>
+</template>
+<script>
+export default {
+ name: 'hello-metamask'
+}
+</script>
+<style scoped>
+</style>
