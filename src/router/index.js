d9fcc97dcb5bd0cdd085817db37410366236fe1f
kiiruha <k.vozniy@gmail.com>
2018-07-28 19:53:56 +0300

    Added networks.js && index.js && state.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'casino-dapp',
+      component: CasinoDApp
     }
   ]
 })
