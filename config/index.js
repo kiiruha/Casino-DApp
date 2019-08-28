e4f8d7b6671720e2af7097bd24e205d0d5964abf
kiiruha <k.vozniy@gmail.com>
2019-08-28 13:39:35 +0300

    Added action for deploy build into gh-pages branch

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Casino-DApp/', //TODO: Delete this fix in feature
 
     /**
      * Source Maps
